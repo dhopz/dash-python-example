92bf7489e55806e608e2b9a061a19745a8325e1c
dhopz <david_houpapa@hotmail.com>
2022-02-04 11:07:17 +0000

    adding packages

---
+++ app.py
@@ -0,0 +1,10 @@
+import dash
+from dash.dependencies import Input, Output, State
+import dash_table
+import dash_core_components as dcc
+import dash_html_components as html
+import plotly.graph_objects as go
+import random
+import plotly.express as px
+import pandas as pd
+import dash_bootstrap_components as dbc

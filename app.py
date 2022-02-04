45df3eac945fa6fda0a6120ee9ea7201ff2e23d7
dhopz <david_houpapa@hotmail.com>
2022-02-04 11:18:39 +0000

    adding second page example

--- app.py
+++ app.py
@@ -121,6 +121,30 @@ def display_output(rows):
 
     return fig
 
+#App Layout
+page_1_layout = html.Div([
+    html.H1('Page 1'),
+    dcc.Dropdown(
+        id='page-1-dropdown',
+        options=[{'label': i, 'value': i} for i in ['LA', 'NYC', 'MTL']],
+        value='LA'
+    ),
+    html.Div(id='page-1-content'),
+    html.Br(),
+    dcc.Link('Go to Page 2', href='/page-2'),
+    html.Br(),
+    dcc.Link('Go back to home', href='/'),
+])
+
+@app.callback(dash.dependencies.Output('page-1-content', 'children'),
+              [dash.dependencies.Input('page-1-dropdown', 'value')])
+def page_1_dropdown(value):
+    return 'You have selected "{}"'.format(value)
+
+
+@app.callback(dash.dependencies.Output('page-content', 'children'),
+              [dash.dependencies.Input('url', 'pathname')])
+
 def render_page_content(pathname):
     if pathname == "/":
         return home_page_layout
